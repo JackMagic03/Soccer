d833bd10894804d3eb8e673200b1148081c2a302
Massimo Gioradno <massimo.giord@gmail.com>
2020-01-30 15:58:27 +0100

    A bello

---
+++ src/mors_pin.ino
@@ -0,0 +1,11 @@
+/**
+ * I pin del calciatore attualmente sono:
+ *    Motori:
+ *      uint8_t pinA[3]   => {13, 7, 4}   giallo
+ *      uint8_t pinB[3]   => {12, 6, 3}   rosso
+ *      uint8_t pinPwm[3] => {11, 5, 2}   bianco
+ *
+ *    Bussola:
+ *      scl_compass 21
+ *      sda_compass 20
+*/
