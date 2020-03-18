07a0c89cdeef1b82048410c106747e10c8b38713
Massimo Gioradno <massimo.giord@gmail.com>
2020-03-18 12:53:34 +0100

    Ufficiale, per ora

--- Programmi Robot ICARO/The_Best_Soccer/mors_globals.h
+++
@@ -1,9 +0,0 @@
-#pragma once;
-
-#include <Arduino.h>
-#include "mors_joints.h"
-#include "mors_drive.h"
-#include "mors_params.h"
-
-extern PhoenixJoints joints[NUM_JOINTS];
-extern PhoenixDrive drive;
