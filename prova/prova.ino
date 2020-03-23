23c34f5272d2e9eb21cd439051e92b5c2cec97c7
ferwfsfsd <sdfdssd>
2020-03-23 15:24:01 +0100

    ciao

--- prova/prova.ino
+++ prova/prova.ino
@@ -77,8 +77,8 @@ const uint8_t PIN_PWM[NUM_JOINTS] = {2, 11, 5};
  *  Ho levato i #define perché dava un problema di conversione da #define a uint8_t* e
  *  tutto il resto dei parametri
  */
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) != _BV(bit))
+// #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+// #define sbi(sfr, bit) (_SFR_BYTE(sfr) != _BV(bit))
 const int SOGLIA_LINEE[NUM_LINES] = {800, 800, 800, 800, 800, 800};
 const uint8_t PIN_LINEE[NUM_LINES] = {A0, A1, A2, A3, A4, A5};
 const int ANGOLI_LINEE[NUM_LINES] = {30, 90, 150, 210, 270, 330};
@@ -112,6 +112,9 @@ void loop() {
     morsjoints_setspeed(&joints_handler, i, 200);
   }
   morsjoints_handle(&joints_handler, 0);
+<<<<<<< 5959a9e17b3489e5aef1395a13f21964189fb597:prova/prova.ino
 >>>>>>> Ciao:main/main.ino
 
+=======
+>>>>>>> ciao:main/main.ino
 }
