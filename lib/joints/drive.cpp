e538e0b6752d753052ac848769ce136b6475ed7a
ferwfsfsd <sdfdssd>
2020-05-08 16:15:48 +0200

    Ciao

--- lib/joints/drive.cpp
+++ lib/joints/drive.cpp
@@ -10,8 +10,8 @@ Drive::Drive() {
    * Inizializziamo le variabili del drive
    */
   for(int i = 0; i < NUM_JOINTS; i++) {
-    
-    joint_component[i].init(PIN_DIR_A[i], PIN_DIR_B[i], PIN_PWM[i]);
+
+    joint_component[i].init(&PIN_DIR_A[i], &PIN_DIR_B[i], &PIN_PWM[i]);
   }
 
   v_x = 0;
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
  * @author Massimo Giordano
  */
 
-#include <Arduino.h>
 #include "globals.hpp"
 
 void setup() {
@@ -14,10 +13,10 @@ void loop() {
 
   //line_handler.handle();
 
-  imu_handler.handle();
+  //imu_handler.handle();
 
-  drive_handler.handle(200, 0, imu_handler.getOutput());
+  //drive_handler.handle(200, 0, imu_handler.getOutput());
 
-  //drive_handler.joint_component[0].handle(200);
+  drive_handler.joint_component[0].handle(200);
   //Serial.println(imu_handler.imu_component.getHeading());
 }
