f1712336997973948cdb2c36ef9fe6ba89d64f56
ferwfsfsd <sdfdssd>
2020-05-08 16:07:23 +0200

    Ciao

--- src/main.cpp
+++ src/main.cpp
@@ -14,10 +14,10 @@ void loop() {
 
   //line_handler.handle();
 
-  //drive_handler.handle(200, line_handler.getEscapeAngle(), imu_handler.getOutput());
+  imu_handler.handle();
+
+  drive_handler.handle(200, 0, imu_handler.getOutput());
 
   //drive_handler.joint_component[0].handle(200);
-
-  imu_handler.imu_component.read();
-  Serial.println(imu_handler.imu_component.getHeading());
+  //Serial.println(imu_handler.imu_component.getHeading());
 }
