9a01ba8c8b4fddfaf5f9c5d4bc0dd1cee3c77408
PC Massimo_Giordano <massimo.giord@gmail.com>
2020-06-02 22:57:08 +0200

    Ciao

--- Librerie Utili/Keypad/examples/CustomKeypad/CustomKeypad.ino
+++ Librerie Utili/Keypad/examples/CustomKeypad/CustomKeypad.ino
@@ -22,15 +22,15 @@ byte rowPins[ROWS] = {9, 8, 7, 6}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {5, 4, 3, 2}; //connect to the column pinouts of the keypad
 
 //initialize an instance of class NewKeypad
-Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
 void setup(){
   Serial.begin(9600);
 }
-  
+
 void loop(){
   char customKey = customKeypad.getKey();
-  
+
   if (customKey){
     Serial.println(customKey);
   }
