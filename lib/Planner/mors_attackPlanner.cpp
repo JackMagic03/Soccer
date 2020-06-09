564e9ce8455f1a70fd29ae995f9e7e310121a1d3
MassimoGiordano03 <massimo.giord@gmail.com>
2020-06-09 23:20:24 +0200

    Aggiunto il Planner

---
+++ lib/Planner/mors_attackPlanner.cpp
@@ -0,0 +1,18 @@
+/**
+ * mors_attackPlanner.cpp
+ */
+#include "mors_attackPlanner.hpp"
+
+MorsAttackPlanner::MorsAttackPlanner()
+{
+	this-> ready = false;
+
+	this-> angle = 0;
+	this-> vel = 0;
+	this-> imu_angle = 0;
+}
+
+MorsAttachPlanner::handle()
+{
+	
+}
---
+++ lib/Planner/mors_defensePlanner.cpp
@@ -0,0 +1,18 @@
+/**
+ * mors_defensePlanner.cpp
+ */
+#include "mors_defensePlanner.cpp"
+
+MorsDefensePlanner::MorsDefensePlanner()
+{
+	this-> ready = false;
+
+	this-> angle = 0;
+	this-> vel = 0;
+	this-> imu_angle = 0;
+}
+
+MorsDefensePlanner::handle()
+{
+	
+}
